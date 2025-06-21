9537ea40af998d536f04d8b8943fd0eec8d6bf79
DilipLovevanshi <dilip@aperasoftwares.com>
2025-06-21 17:11:30 +0530

    fixed the build error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,10 +15,10 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-          "react-hooks/exhaustive-deps": "off"
-
-    }
-  }
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-img-element": "off", // ✅ disables <img> warning from Next.js
+    },
+  },
 ];
 
 export default eslintConfig;
