e1389e648391680958f529ffc234dec40d611eb6
Sonawane-Shubham <sssonawane047@gmail.com>
2025-06-21 16:31:12 +0530

    change backend url

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
-//export const BACKEND_API ="http://209.182.232.11:4048/"
-export const BACKEND_API ="https://2e2f-203-192-225-53.ngrok-free.app/"
+export const BACKEND_API ="http://209.182.232.11:4048/"
+//export const BACKEND_API ="https://2e2f-203-192-225-53.ngrok-free.app/"
