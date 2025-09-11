8d034f0a44e0799dd450b97db1ade6102c469755
Sonawane-Shubham <sssonawane047@gmail.com>
2025-09-11 17:44:59 +0530

    Integrated redux-persist to persist the pageTitle state across page reloads

--- src/lib/redux/store.tsx
+++ src/lib/redux/store.tsx
@@ -1,35 +1,63 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+//import storage from "redux-persist/lib/storage"; // defaults to localStorage
+import storage from "@/utils/storage";
 import userReducer from "./slices/userSlice";
 import userManagementReducer from "./slices/userManagementSlice";
 import productCatalogReducer from "./slices/productCatalogSlice";
 import teamManagementReducer from "./slices/teamManagementSlice";
 import userProfileReducer from "./slices/loginPersonProfile";
-import memberReducer from './slices/memberSlice';
+import memberReducer from "./slices/memberSlice";
 import referralReducer from "./slices/referralSlice";
 import statisticsReducer from "./slices/statisticsSlice";
 import appReducer from "./slices/appSlice";
 import authReducer from "./slices/authSlice";
 
+const rootReducer = combineReducers({
+  app: appReducer,
+  auth: authReducer,
+  user: userReducer,
+  userManagement: userManagementReducer,
+  productCatalog: productCatalogReducer,
+  teamManagement: teamManagementReducer,
+  userProfile: userProfileReducer,
+  member: memberReducer,
+  referral: referralReducer,
+  statistics: statisticsReducer,
+});
+
+// persist only the `app` slice
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["app"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const makeStore = () => {
   return configureStore({
-    reducer: {
-      app:appReducer,
-      auth:authReducer,
-      user: userReducer,
-      userManagement: userManagementReducer,
-      productCatalog: productCatalogReducer,
-      teamManagement: teamManagementReducer,
-      userProfile: userProfileReducer,
-      member: memberReducer,
-      referral:referralReducer,
-      statistics:statisticsReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 };
 
-// Create the store instance (optional: used outside of SSR)
 export const store = makeStore();
+export const persistor = persistStore(store);
 
-// Infer the `RootState` and `AppDispatch` types from the store
-export type AppDispatch = ReturnType<typeof makeStore>["dispatch"];
-export type RootState = ReturnType<ReturnType<typeof makeStore>["getState"]>;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
