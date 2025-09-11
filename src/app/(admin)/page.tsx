eefcf59afa6403a8e15e4f525b6412cb2ab9bdfb
Sonawane-Shubham <sssonawane047@gmail.com>
2025-09-11 18:13:16 +0530

    setpagetitle on home page laod

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from "react";
-import { useAppSelector} from "@/lib/redux/hooks";
+import React,{useEffect} from "react";
+import { useAppDispatch,useAppSelector} from "@/lib/redux/hooks";
+import { setPageTitle } from "@/lib/redux/slices/appSlice";
 import { UserRole } from "@/constant/userRoles";
 import AdminDashboard from "@/components/dashboard/AdminDashboard";
 import ATeamDashboard from "@/components/dashboard/ATeamDashboard";
@@ -8,7 +9,12 @@ import BTeamDashboard from "@/components/dashboard/BTeamDashboard";
 import ReferralTable from "@/components/referral/ReferralTable";
 
 export default function Dashboard() {
+  const dispatch = useAppDispatch();
   const { user: loggedInUser } = useAppSelector((state) => state.user);
+
+  useEffect(()=>{
+     dispatch(setPageTitle("Dashboard"));
+  },[])
 
   return (
     <div className="w-full">
